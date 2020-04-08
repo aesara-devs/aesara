d46fd7b2e0e7f7f6e4dbcaecd0bc3c40c2299fbd
Dan F-M <foreman.mackey@gmail.com>
2020-04-08 09:19:36 -0400

    conda activate issues on travis

--- .travis/travis_before_install.sh
+++ .travis/travis_before_install.sh
@@ -8,5 +8,16 @@ else
     mkdir -p $HOME/download
     if [[ -d $HOME/download/miniconda.sh ]] ; then rm -rf $HOME/download/miniconda.sh ; fi
     wget -c https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/download/miniconda.sh
+
+    mkdir $HOME/.conda
     bash $HOME/download/miniconda.sh -b -p $HOME/miniconda
 fi
+
+$HOME/miniconda/bin/conda init bash
+source ~/.bash_profile
+conda activate base
+
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda info -a
-
-if test -e $HOME/miniconda/envs/pyenv; then
-    echo "pyenv already exists."
-else
-    echo "Creating pyenv."
-    conda create --yes -q -n pyenv python=$TRAVIS_PYTHON_VERSION
-fi
-
+conda create --yes -q -n pyenv python=$TRAVIS_PYTHON_VERSION
 conda activate pyenv
 conda install --yes -q mkl numpy scipy nose pip flake8 six pep8 pyflakes sphinx mkl-service libgfortran graphviz
 python -m pip install -q pydot-ng flake8-future-import parameterized sphinx_rtd_theme nose-exclude nose-timer
