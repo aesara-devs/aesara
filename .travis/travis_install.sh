e76727b6cc46be97da72d045297d81a1ea617bb2
Dan F-M <foreman.mackey@gmail.com>
2020-04-08 08:58:31 -0400

    conda activate

--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -7,8 +7,8 @@ else
     conda create --yes -q -n pyenv python=$TRAVIS_PYTHON_VERSION
 fi
 
-source activate pyenv
+conda activate pyenv
 conda install --yes -q mkl numpy scipy nose pip flake8 six pep8 pyflakes sphinx mkl-service libgfortran graphviz
 python -m pip install -q pydot-ng flake8-future-import parameterized sphinx_rtd_theme nose-exclude nose-timer
 python -m pip install --no-deps --upgrade -e .
-source deactivate
+conda deactivate
