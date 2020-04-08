4ce5b9d98d91d458a41c4998f1841395520a2a51
Dan F-M <foreman.mackey@gmail.com>
2020-04-08 08:53:58 -0400

    updating travis build for Python 3

--- .travis/travis_before_install.sh
+++ .travis/travis_before_install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # Install miniconda to avoid compiling scipy
-if test -e $HOME/miniconda2/bin ; then
+if test -e $HOME/miniconda/bin ; then
     echo "miniconda already installed."
 else
     echo "Installing miniconda."
-    rm -rf $HOME/miniconda2
+    rm -rf $HOME/miniconda
     mkdir -p $HOME/download
     if [[ -d $HOME/download/miniconda.sh ]] ; then rm -rf $HOME/download/miniconda.sh ; fi
-    wget -c https://repo.continuum.io/miniconda/Miniconda2-4.5.11-Linux-x86_64.sh -O $HOME/download/miniconda.sh
+    wget -c https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/download/miniconda.sh
     chmod +x $HOME/download/miniconda.sh
-    $HOME/download/miniconda.sh -b
+    $HOME/download/miniconda.sh -b -p $HOME/miniconda
 fi
--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 # In Python 3.4, we test the min version of NumPy and SciPy. In Python 2.7, we test more recent version.
-if test -e $HOME/miniconda2/envs/pyenv; then
+if test -e $HOME/miniconda/envs/pyenv; then
     echo "pyenv already exists."
 else
     echo "Creating pyenv."
-    if [[ $TRAVIS_PYTHON_VERSION == '2.7' ]]; then conda create --yes -q -n pyenv python=2.7 ; fi
-    if [[ $TRAVIS_PYTHON_VERSION == '3.4' ]]; then conda create --yes -q -n pyenv python=3.4 ; fi
-    if [[ $TRAVIS_PYTHON_VERSION == '3.6' ]]; then conda create --yes -q -n pyenv python=3.6 ; fi
+    conda create --yes -q -n pyenv python=$TRAVIS_PYTHON_VERSION
 fi
 
 source activate pyenv
-if [[ $TRAVIS_PYTHON_VERSION == '2.7' && $NUMPY_VERSION == '1.9.1' ]]; then conda install --yes -q mkl numpy=1.9.1 scipy=0.14.0 nose=1.3.0 pip flake8=2.3 six=1.9.0 pep8=1.6.2 pyflakes=0.8.1 sphinx=1.5.1 mkl-service libgfortran=1 graphviz; fi
-if [[ $TRAVIS_PYTHON_VERSION == '2.7' && $NUMPY_VERSION == '1.13.1' ]]; then conda install --yes -q mkl numpy=1.13.1 scipy=0.19.1 nose=1.3.0 pip flake8=2.3 six=1.9.0 pep8=1.6.2 pyflakes=0.8.1 sphinx=1.5.1 mkl-service libgfortran=3 graphviz; fi
-if [[ $TRAVIS_PYTHON_VERSION == '3.4' && $NUMPY_VERSION == '1.9.1' ]]; then conda install --yes -q mkl numpy=1.9.1 scipy=0.14.0 nose=1.3.4 pip flake8=2.3 six=1.9.0 pep8=1.6.2 pyflakes=0.8.1 sphinx=1.5.1 mkl-service libgfortran=1 graphviz pygments=2.1.3; fi
-if [[ $TRAVIS_PYTHON_VERSION == '3.6' && $NUMPY_VERSION == '1.13.1' ]]; then conda install --yes -q mkl numpy=1.13.1 scipy=0.19.1 nose=1.3.7 pip flake8=3.5 six=1.11.0 pep8=1.7.1 pyflakes=1.6.0 sphinx=1.5.1 mkl-service libgfortran=3 graphviz; fi
+conda install --yes -q mkl numpy scipy nose pip flake8 six pep8 pyflakes sphinx mkl-service libgfortran graphviz
+python -m pip install -q pydot-ng flake8-future-import parameterized sphinx_rtd_theme nose-exclude nose-timer
+python -m pip install --no-deps --upgrade -e .
 source deactivate
