29590c5051cd1f3b25b5079a442c767702c529e7
Dan F-M <foreman.mackey@gmail.com>
2020-04-08 10:57:28 -0400

    need old sphinx version

--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -5,8 +5,8 @@ set -e
 if [[ $DOC == "1" ]]; then
 conda create --yes -q -n pyenv python=3.6 numpy=1.13.1
 conda activate pyenv
-conda install --yes -q mkl numpy=1.13.1 scipy=0.19.1 nose=1.3.7 pip flake8=3.5 six=1.11.0 pep8=1.7.1 pyflakes=1.6.0 sphinx mkl-service graphviz
-python -m pip install pydot-ng flake8-future-import parameterized nose-exclude nose-timer
+conda install --yes -q mkl numpy=1.13.1 scipy=0.19.1 nose=1.3.7 pip flake8=3.5 six=1.11.0 pep8=1.7.1 pyflakes=1.6.0 mkl-service graphviz
+python -m pip install pydot-ng flake8-future-import parameterized nose-exclude nose-timer sphinx=1.5.1
 else
 conda create --yes -q -n pyenv python=$TRAVIS_PYTHON_VERSION
 conda activate pyenv
