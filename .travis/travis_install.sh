6a4cf4c96cd284e5ac4d22b6df88d8718c1d8a73
Dan F-M <foreman.mackey@gmail.com>
2020-04-08 11:07:18 -0400

    sphinx version

--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -3,10 +3,12 @@
 set -e
 
 if [[ $DOC == "1" ]]; then
+# this is a hack to deal with the fact that the docs and flake8 config are all set up
+# for old versions
 conda create --yes -q -n pyenv python=3.6 numpy=1.13.1
 conda activate pyenv
 conda install --yes -q mkl numpy=1.13.1 scipy=0.19.1 nose=1.3.7 pip flake8=3.5 six=1.11.0 pep8=1.7.1 pyflakes=1.6.0 mkl-service graphviz
-python -m pip install pydot-ng flake8-future-import parameterized nose-exclude nose-timer sphinx=1.5.1
+python -m pip install pydot-ng flake8-future-import parameterized nose-exclude nose-timer sphinx==1.5.1
 else
 conda create --yes -q -n pyenv python=$TRAVIS_PYTHON_VERSION
 conda activate pyenv
