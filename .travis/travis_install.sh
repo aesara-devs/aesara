11c78375001575ec20928573d641e0a17ee1cbb2
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2020-07-31 17:54:42 -0500

    Refactor travis config and add Coveralls

--- .travis/travis_install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-conda create --yes -q -n pyenv python=$TRAVIS_PYTHON_VERSION
-conda activate pyenv
-conda install --yes -q mkl numpy scipy pip flake8 six pep8 pyflakes sphinx mkl-service graphviz pytest  # libgfortran
-python -m pip install -q pydot-ng sphinx_rtd_theme
-python -m pip install --no-deps --upgrade -e .
