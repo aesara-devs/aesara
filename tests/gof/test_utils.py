e2dd22c4d8d08799e6f58add8e3b7b4573cecc80
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2020-12-03 23:34:54 -0600

    Remove deprecated and unused functions in theano.gof.test_utils

--- tests/gof/test_utils.py
+++ tests/gof/test_utils.py
@@ -1,38 +1,5 @@
 import theano
-from theano.gof.utils import give_variables_names, remove, unique
-
-
-def test_give_variables_names():
-    x = theano.tensor.matrix("x")
-    y = x + 1
-    z = theano.tensor.dot(x, y)
-    variables = (x, y, z)
-    give_variables_names(variables)
-    assert all(var.name for var in variables)
-    assert unique([var.name for var in variables])
-
-
-def test_give_variables_names_idempotence():
-    x = theano.tensor.matrix("x")
-    y = x + 1
-    z = theano.tensor.dot(x, y)
-    variables = (x, y, z)
-
-    give_variables_names(variables)
-    names = [var.name for var in variables]
-    give_variables_names(variables)
-    names2 = [var.name for var in variables]
-
-    assert names == names2
-
-
-def test_give_variables_names_small():
-    x = theano.tensor.matrix("x")
-    y = theano.tensor.dot(x, x)
-    fgraph = theano.FunctionGraph((x,), (y,))
-    give_variables_names(fgraph.variables)
-    assert all(var.name for var in fgraph.variables)
-    assert unique([var.name for var in fgraph.variables])
+from theano.gof.utils import remove
 
 
 def test_remove():
