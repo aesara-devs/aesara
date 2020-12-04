d0f80b0bf27f0c0c28792a785752083a8a81e4ef
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2020-12-03 23:37:40 -0600

    Remove deprecated theano.sandbox modules

--- theano/sandbox/conv.py
+++
@@ -1,8 +0,0 @@
-from warnings import warn
-
-
-warn(
-    "theano.sandbox.conv no longer provides conv. "
-    "They have been moved to theano.tensor.nnet.conv",
-    category=DeprecationWarning,
-)
--- theano/sandbox/softsign.py
+++
@@ -1,8 +0,0 @@
-from theano.tensor.nnet.nnet import softsign  # noqa
-from warnings import warn
-
-
-warn(
-    "softsign was moved from theano.sandbox.softsign to theano.tensor.nnet.nnet ",
-    category=DeprecationWarning,
-)
