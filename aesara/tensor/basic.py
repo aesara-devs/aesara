d4d7f9569eebb295175dc47e2e45d3cf73240957
George Ho <19851673+eigenfoo@users.noreply.github.com>
2021-02-07 12:08:09 -0500

    Add support for BroadcastTo in get_scalar_constant_value
    This change allows `get_scalar_constant_value` to "dig through" `BroadcastTo`
    `Op`s.

--- aesara/tensor/basic.py
+++ aesara/tensor/basic.py
@@ -324,6 +324,7 @@ def get_scalar_constant_value(
                     Alloc,
                     DimShuffle,
                     Rebroadcast,
+                    aesara.tensor.extra_ops.BroadcastTo,
                     # outputguard is only used in debugmode but we
                     # keep it here to avoid problems with old pickels.
                     compile.ops.OutputGuard,
