f5755c5e6ae4daa1b4acbc8a74bfa5495b35cc39
kc611 <ckaustubhm06@gmail.com>
2021-04-13 21:49:34 +0530

    Generalize Numba conversion of Scalar Ops

--- tests/link/test_numba.py
+++ tests/link/test_numba.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 import aesara
+import aesara.scalar.basic as aes
 import aesara.tensor as aet
 from aesara.compile.mode import Mode
 from aesara.graph.optdb import Query
@@ -15,7 +16,7 @@ numba_mode = Mode(NumbaLinker(), opts)
 py_mode = Mode("py", opts)
 
 
-def test_composite():
+def test_Composite():
     y = aet.vector("y")
     x = aet.vector("x")
 
@@ -23,6 +24,10 @@ def test_composite():
 
     func = aesara.function([x, y], [z], mode=py_mode)
     numba_fn = aesara.function([x, y], [z], mode=numba_mode)
+
+    # Make sure the graph had a `Composite` `Op` in it
+    composite_op = numba_fn.maker.fgraph.outputs[0].owner.op.scalar_op
+    assert isinstance(composite_op, aes.Composite)
 
     x_val = np.random.randn(1000)
     y_val = np.random.randn(1000)
@@ -32,18 +37,25 @@ def test_composite():
 
     assert np.array_equal(res, numba_res)
 
-    # y1 = aet.vector("y1")
-    # x1 = aet.vector("x1")
+    y1 = aet.vector("y1")
+    x1 = aet.vector("x1")
 
-    # z = (x + y) * (x1 + y1) * y
+    z = (x + y) * (x1 + y1) * y
 
-    # x1_val = np.random.randn(1000)
-    # y1_val = np.random.randn(1000)
+    x1_val = np.random.randn(1000)
+    y1_val = np.random.randn(1000)
 
-    # func = aesara.function([x, y, x1, y1], [z], mode=mode)
-    # numba_fn = compile_graph(func.maker.fgraph, debug=True)
+    func = aesara.function([x, y, x1, y1], [z], mode=py_mode)
+    numba_fn = aesara.function([x, y, x1, y1], [z], mode=numba_mode)
 
-    # res = func(x_val, y_val, x1_val, y1_val)  # Answer from python mode compilation of FunctionGraph
-    # numba_res = numba_fn(x_val, y_val,x1_val,y1_val)  # Answer from Numba converted FunctionGraph
+    composite_op = numba_fn.maker.fgraph.outputs[0].owner.op.scalar_op
+    assert isinstance(composite_op, aes.Composite)
 
-    # assert np.array_equal(res, numba_res)
+    res = func(
+        x_val, y_val, x1_val, y1_val
+    )  # Answer from python mode compilation of FunctionGraph
+    numba_res = numba_fn(
+        x_val, y_val, x1_val, y1_val
+    )  # Answer from Numba converted FunctionGraph
+
+    assert np.array_equal(res, numba_res)
