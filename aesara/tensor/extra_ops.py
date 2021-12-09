Generalize broadcastables inference and fix Alloc broadcastables case
Closes #692

aesara/tensor/extra_ops.py
@@ -1585,7 +1585,7 @@ class BroadcastTo(Op):
         a = aet.as_tensor_variable(a)
         shape = aet.as_tensor_variable(shape, ndim=1)
 
-        shape, bcast = aet.alloc_validate_shape(shape)
+        shape, bcast = aet.infer_broadcastable(shape)
 
         out = type(a.type)(dtype=a.type.dtype, broadcastable=bcast)()
 
@@ -1609,7 +1609,7 @@ class BroadcastTo(Op):
         d_wrt_a = broadcast_to(dout, shape).sum(axis=new_dims)
 
         # Determine the dimensions that were broadcast
-        _, shape_bcast = aet.alloc_validate_shape(shape)
+        _, shape_bcast = aet.infer_broadcastable(shape)
         bcast_sums = [
             i
             for i, (a_b, s_b) in enumerate(zip(a.broadcastable, shape_bcast[-a.ndim :]))

tests/tensor/test_basic.py
@@ -59,6 +59,7 @@ from aesara.tensor.basic import (
     get_scalar_constant_value,
     get_vector_length,
     horizontal_stack,
+    infer_broadcastable,
     inverse_permutation,
     join,
     make_vector,
@@ -90,7 +91,7 @@ from aesara.tensor.elemwise import DimShuffle
 from aesara.tensor.exceptions import NotScalarConstantError
 from aesara.tensor.math import dense_dot, eq
 from aesara.tensor.math import sum as aet_sum
-from aesara.tensor.shape import Reshape, Shape, Shape_i, shape_padright
+from aesara.tensor.shape import Reshape, Shape, Shape_i, shape_padright, specify_shape
 from aesara.tensor.type import (
     TensorType,
     bvector,
@@ -656,6 +657,24 @@ class TestAlloc:
         full_at = aet.full((2, 3), 3, dtype="int64")
         res = aesara.function([], full_at, mode=self.mode)()
         assert np.array_equal(res, np.full((2, 3), 3, dtype="int64"))
+
+
+def test_infer_broadcastable():
+    with pytest.raises(TypeError, match="^Shapes must be scalar integers.*"):
+        infer_broadcastable([constant(1.0)])
+
+    with config.change_flags(exception_verbosity="high"), pytest.raises(
+        TypeError, match=r"A\. x"
+    ):
+        infer_broadcastable([dscalar("x")])
+
+    with pytest.raises(ValueError, match=".*could not be cast to have 0 dimensions"):
+        infer_broadcastable((as_tensor_variable([[1, 2]]),))
+
+    constant_size = constant([1])
+    specify_size = specify_shape(constant_size, [1])
+    sh, bcast = infer_broadcastable(specify_size)
+    assert bcast == (True,)
 
 
 # This is slow for the ('int8', 3) version.