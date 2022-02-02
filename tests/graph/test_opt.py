aesara-devs/aesara

commit b920bd2910e42e72b79dbb3a353505a09b84241e

Use "class"-based comparisons between Types
These changes enforce a strict "narrowing"-only conversion policy; i.e. `Type`s
can be converted to equal or more specific types.

aesara/graph/opt.py
@@ -1762,14 +1762,15 @@ class PatternSub(LocalOptimizer):
             ret.tag.values_eq_approx = self.values_eq_approx
 
         if ret.owner:
-            if [out.type for out in ret.owner.outputs] != [
-                out.type for out in node.outputs
-            ]:
+            if not all(
+                o.type.is_super(new_o.type)
+                for o, new_o in zip(node.outputs, ret.owner.outputs)
+            ):
                 return False
         else:
             # ret is just an input variable
             assert len(node.outputs) == 1
-            if ret.type != node.outputs[0].type:
+            if not node.outputs[0].type.is_super(ret.type):
                 return False
 
         return [ret]

tests/graph/test_opt.py
@@ -649,7 +649,7 @@ def test_patternsub_invalid_dtype(out_pattern):
         max_use_ratio=1,
     )
     opt.optimize(fg)
-    assert fg.apply_nodes.pop().op == op_cast_type2
+    assert e.type.is_super(fg.outputs[0].type)
 
 
 class TestLocalOptGroup:

aesara/gpuarray/opt.py
@@ -458,7 +458,7 @@ class GraphToGPU(GlobalOptimizer):
                 if (
                     new_o.owner
                     and isinstance(new_o.owner.op, GpuFromHost)
-                    and new_o.owner.inputs[0].type == o.type
+                    and o.type.in_same_class(new_o.owner.inputs[0].type)
                 ):
                     new_o = new_o.owner.inputs[0]
                 else:

aesara/scan/opt.py
@@ -308,7 +308,7 @@ def push_out_non_seq_scan(fgraph, node):
     for out, idx in to_replace_map.items():
         if (  # If types are different, conversion Op will be inserted,
             # and it may trigger an infinite loop.
-            replace_with_in[idx].type == out.type
+            out.type.is_super(replace_with_in[idx].type)
             and out in to_keep_set
             and out.owner not in existent_nodes_set
         ):
@@ -557,7 +557,7 @@ def push_out_seq_scan(fgraph, node):
             and
             # If types are different, conversion Op will be inserted,
             # and it may trigger an infinite loop.
-            replace_with_in[idx].type == out.type
+            out.type.is_super(replace_with_in[idx].type)
         ):
 
             clean_to_replace.append(out)

aesara/compile/debugmode.py
@@ -715,7 +715,7 @@ def _find_bad_optimizations(order, reasons, r_vals):
                 # check if the value for new_r doesn't match the value for r
                 new_r_val = r_vals[new_r]
                 r_val = r_vals[r]
-                assert r.type == new_r.type
+                assert r.type.is_super(new_r.type)
 
                 if hasattr(new_r.tag, "values_eq_approx"):
                     check = new_r.tag.values_eq_approx(r_val, new_r_val)

aesara/compile/function/pfunc.py
@@ -111,7 +111,7 @@ def rebuild_collect_shared(
                         v_update = v.type.filter_variable(
                             v.default_update, allow_convert=False
                         )
-                        if v_update.type != v.type:
+                        if not v.type.is_super(v_update.type):
                             raise TypeError(
                                 "An update must have a type compatible with "
                                 "the original shared variable"
@@ -205,7 +205,7 @@ def rebuild_collect_shared(
             )
 
             raise TypeError(err_msg, err_sug)
-        assert update_val.type == store_into.type
+        assert store_into.type.is_super(update_val.type)
 
         update_d[store_into] = update_val
         update_expr.append((store_into, update_val))

aesara/compile/function/types.py
@@ -621,7 +621,7 @@ class Function:
                 "type",
                 type(sv_ori),
             )
-            assert sv_ori.type == sv_rpl.type, (
+            assert sv_rpl.type.in_same_class(sv_ori.type), (
                 "Type of given SharedVariable conflicts with original one",
                 "Type of given SharedVariable:",
                 sv_rpl.type,

aesara/compile/io.py
@@ -79,11 +79,11 @@ class SymbolicInput:
             raise TypeError(f"name must be a string! (got: {self.name})")
         self.update = update
         if update is not None:
-            if variable.type != update.type:
+            if not variable.type.in_same_class(update.type):
                 raise TypeError(
                     f"Variable '{variable}' has type {variable.type} but an update of "
                     f"type {update.type}. The type of the update should be "
-                    "the same as the type of the variable"
+                    "compatible with the type of the variable."
                 )
 
         if mutable is not None:

commit bb8a8b450433f239fead5885d84ef757982dccc4

Fix bug where PatternSub could incorrectly replace nodes with different number of outputs
Bug was introduced in b920bd2910e42e72b79dbb3a353505a09b84241e

aesara/graph/opt.py
@@ -1762,9 +1762,12 @@ class PatternSub(LocalOptimizer):
             ret.tag.values_eq_approx = self.values_eq_approx
 
         if ret.owner:
-            if not all(
-                o.type.is_super(new_o.type)
-                for o, new_o in zip(node.outputs, ret.owner.outputs)
+            if not (
+                len(node.outputs) == len(ret.owner.outputs)
+                and all(
+                    o.type.is_super(new_o.type)
+                    for o, new_o in zip(node.outputs, ret.owner.outputs)
+                )
             ):
                 return False
         else:

tests/graph/test_opt.py
@@ -14,6 +14,7 @@ from aesara.graph.opt import (
     OpSub,
     PatternSub,
     TopoOptimizer,
+    in2out,
     local_optimizer,
     logging,
     pre_constant_merge,
@@ -36,6 +37,7 @@ from tests.graph.utils import (
     op5,
     op6,
     op_cast_type2,
+    op_multiple_outputs,
     op_y,
     op_z,
 )
@@ -650,6 +652,24 @@ def test_patternsub_invalid_dtype(out_pattern):
     )
     opt.optimize(fg)
     assert e.type.is_super(fg.outputs[0].type)
+
+
+def test_patternsub_different_output_lengths():
+    # Test that PatternSub won't replace nodes with different numbers of outputs
+    ps = PatternSub(
+        (op1, "x"),
+        ("x"),
+        name="ps",
+    )
+    opt = in2out(ps)
+
+    x = MyVariable("x")
+    e1, e2 = op_multiple_outputs(x)
+    o = op1(e1)
+
+    fgraph = FunctionGraph(inputs=[x], outputs=[o])
+    opt.optimize(fgraph)
+    assert fgraph.outputs[0].owner.op == op1
 
 
 class TestLocalOptGroup:

tests/graph/utils.py
@@ -96,6 +96,12 @@ class MyOpCastType2(MyOp):
         return Apply(self, inputs, outputs)
 
 
+class MyOpMultipleOutputs(MyOp):
+    def make_node(self, input):
+        outputs = [input.type(), input.type()]
+        return Apply(self, [input], outputs)
+
+
 op1 = MyOp("Op1")
 op2 = MyOp("Op2")
 op3 = MyOp("Op3")
@@ -108,6 +114,7 @@ op_y = MyOp("OpY", x=1)
 op_z = MyOp("OpZ", x=1)
 
 op_cast_type2 = MyOpCastType2("OpCastType2")
+op_multiple_outputs = MyOpMultipleOutputs("OpMultipleOutputs")
 
 
 class MyInnerGraphOp(Op, HasInnerGraph):