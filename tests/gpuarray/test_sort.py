ff49a277ea1b74dc39826542d278c59115e228b5
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2022-03-11 17:42:52 +0000

    Remove tests.gpuarray

--- tests/gpuarray/test_sort.py
+++
@@ -1,9 +0,0 @@
-from aesara.gpuarray.sort import GpuTopKOp
-from tests.gpuarray.config import mode_with_gpu
-from tests.tensor.test_sort import TestTopK
-
-
-class TestGpuTopK(TestTopK):
-    mode = mode_with_gpu
-    dtype = "float32"
-    op_class = GpuTopKOp
