b3ce36404e96c8e9dc4500184b47d495f7f4862d
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2022-03-11 17:42:52 +0000

    Remove tests.gpuarray

--- tests/gpuarray/test_neighbours.py
+++
@@ -1,9 +0,0 @@
-from aesara.gpuarray.neighbours import GpuImages2Neibs
-from tests.gpuarray.config import mode_with_gpu
-from tests.tensor.nnet import test_neighbours
-
-
-class TestGpuImages2Neibs(test_neighbours.TestImages2Neibs):
-    mode = mode_with_gpu
-    op = GpuImages2Neibs
-    dtypes = ["int64", "float32", "float64"]
