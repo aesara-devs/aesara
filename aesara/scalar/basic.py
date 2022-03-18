327a95d9b62844230d014b1c36fa29cb1ffa1580
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2022-03-18 16:33:01 -0500

    Add HasDataType mixin to Scalar

--- aesara/scalar/basic.py
+++ aesara/scalar/basic.py
@@ -28,6 +28,7 @@ from aesara.gradient import DisconnectedType, grad_undefined
 from aesara.graph.basic import Apply, Constant, Variable, clone, list_of_nodes
 from aesara.graph.fg import FunctionGraph
 from aesara.graph.opt import MergeOptimizer
+from aesara.graph.type import HasDataType
 from aesara.graph.utils import MetaObject, MethodNotDefined
 from aesara.link.c.op import COp
 from aesara.link.c.type import CType
@@ -274,7 +275,7 @@ def convert(x, dtype=None):
     return x_
 
 
-class Scalar(CType):
+class Scalar(CType, HasDataType):
 
     """
     Internal class, should not be used by clients.
