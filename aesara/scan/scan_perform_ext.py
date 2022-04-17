eafee83aceddecea8ec60e6bc55196619fddb874
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2022-04-17 13:18:47 -0500

    Update the Cython Scan implementation's version number

--- aesara/scan/scan_perform_ext.py
+++ aesara/scan/scan_perform_ext.py
@@ -23,7 +23,7 @@ if not config.cxx:
 
 _logger = logging.getLogger("aesara.scan.scan_perform")
 
-version = 0.312  # must match constant returned in function get_version()
+version = 0.313  # must match constant returned in function get_version()
 
 need_reload = False
 scan_perform: Optional[ModuleType] = None
