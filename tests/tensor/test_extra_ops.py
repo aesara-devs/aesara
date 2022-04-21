55f2d093f30fb5072ca60c3c4c8c71ab51e98331
Larry Dong <larry.dong@mail.utoronto.ca>
2022-04-21 15:22:22 -0400

    fix pre-commit

--- tests/tensor/test_extra_ops.py
+++ tests/tensor/test_extra_ops.py
@@ -310,7 +310,6 @@ class TestDiff(utt.InferShapeTester):
                 g = aesara.function([x], diff(x, n=n, axis=axis))
                 assert np.allclose(np.diff(a, n=n, axis=axis), g(a))
 
-
     @pytest.mark.xfail(reason="Subtensor shape cannot be inferred correctly")
     @pytest.mark.parametrize(
         "x_type",
