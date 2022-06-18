8e8a59ad946e78e25a088c69eacbe469df3b4897
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2022-06-17 22:51:04 -0500

    Enable mypy checking for aesara.tensor.rewriting.shape

--- aesara/tensor/basic_opt.py
+++ aesara/tensor/basic_opt.py
@@ -6,8 +6,3 @@ warnings.warn(
     DeprecationWarning,
     stacklevel=2,
 )
-
-from aesara.tensor.rewriting.basic import *  # noqa: F401 E402 F403
-from aesara.tensor.rewriting.elemwise import *  # noqa: F401 E402 F403
-from aesara.tensor.rewriting.extra_ops import *  # noqa: F401 E402 F403
-from aesara.tensor.rewriting.shape import *  # noqa: F401 E402 F403
