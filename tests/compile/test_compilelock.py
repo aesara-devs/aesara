5ce7f4afbd375e8dc6ff64360248caa54b8d69c4
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2022-06-21 15:33:14 +0000

    add an aggressive test

--- tests/compile/test_compilelock.py
+++ tests/compile/test_compilelock.py
@@ -1,13 +1,18 @@
 import multiprocessing
 import os
+import shutil
 import sys
 import tempfile
 import threading
 import time
 
 import filelock
+import numpy as np
 import pytest
 
+import aesara
+import aesara.compile.compiledir
+import aesara.tensor as at
 from aesara.compile.compilelock import force_unlock, local_mem, lock_ctx
 
 
@@ -131,3 +136,26 @@ def test_locking_multiprocess_fork():
 def test_locking_multiprocess_spawn():
     ctx = multiprocessing.get_context("spawn")
     run_locking_test(ctx)
+
+
+def test_thread_safety():
+    shutil.rmtree(aesara.config.compiledir)
+    os.mkdir(aesara.config.compiledir)
+
+    np.random.seed(32)
+
+    def run_aesara(i, out):
+        s = np.random.randint(10, 50)
+        a = at.random.normal(size=(s, s))
+        fn = aesara.function([], a)
+        for _ in range(10):
+            fn()
+        out[i] = True
+
+    T = 32
+    out = [None] * T
+
+    threads = [threading.Thread(target=run_aesara, args=(i, out)) for i in range(T)]
+    [t.start() for t in threads]
+    [t.join() for t in threads]
+    assert all(out)
