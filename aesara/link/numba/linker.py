971644fbe89621e576f5a4bdeb97282230d97683
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2022-07-22 18:54:04 -0500

    Add output_filter method to JITLinker and use it to convert Numba scalars

--- aesara/link/numba/linker.py
+++ aesara/link/numba/linker.py
@@ -1,8 +1,25 @@
+from typing import TYPE_CHECKING, Any
+
+import numpy as np
+
+import aesara
 from aesara.link.basic import JITLinker
+
+
+if TYPE_CHECKING:
+    from aesara.graph.basic import Variable
 
 
 class NumbaLinker(JITLinker):
     """A `Linker` that JIT-compiles NumPy-based operations using Numba."""
+
+    def output_filter(self, var: "Variable", out: Any) -> Any:
+        if not isinstance(var, np.ndarray) and isinstance(
+            var.type, aesara.tensor.TensorType
+        ):
+            return np.asarray(out, dtype=var.type.dtype)
+
+        return out
 
     def fgraph_convert(self, fgraph, **kwargs):
         from aesara.link.numba.dispatch import numba_funcify
