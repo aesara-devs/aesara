3e5ad9977f6bf526c97c372557d651d43e3724ec
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2022-09-16 17:46:16 -0500

    Refactor SparseTensorType constructor and clone interface

--- tests/sparse/test_type.py
+++ tests/sparse/test_type.py
@@ -1,14 +1,39 @@
+import pytest
+
 from aesara.sparse import matrix as sp_matrix
 from aesara.sparse.type import SparseTensorType
 from aesara.tensor import dmatrix
 
 
-def test_clone():
-    st = SparseTensorType("csr", "float64")
+def test_SparseTensorType_constructor():
+    st = SparseTensorType("csc", "float64")
+    assert st.format == "csc"
+    assert st.shape == (None, None)
+
+    st = SparseTensorType("bsr", "float64", shape=(None, 1))
+    assert st.format == "bsr"
+    assert st.shape == (None, 1)
+
+    with pytest.raises(ValueError):
+        SparseTensorType("blah", "float64")
+
+
+def test_SparseTensorType_clone():
+    st = SparseTensorType("csr", "float64", shape=(3, None))
     assert st == st.clone()
 
+    st_clone = st.clone(format="csc")
+    assert st_clone.format == "csc"
+    assert st_clone.dtype == st.dtype
+    assert st_clone.shape == st.shape
 
-def test_Sparse_convert_variable():
+    st_clone = st.clone(shape=(2, 1))
+    assert st_clone.format == st.format
+    assert st_clone.dtype == st.dtype
+    assert st_clone.shape == (2, 1)
+
+
+def test_SparseTensorType_convert_variable():
     x = dmatrix(name="x")
     y = sp_matrix("csc", dtype="float64", name="y")
     z = sp_matrix("csr", dtype="float64", name="z")
