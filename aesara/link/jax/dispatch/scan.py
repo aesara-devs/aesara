3918d9ff0ad6cd2aa8ab1953af35a47b0d89e9cf
Rémi Louf <remilouf@gmail.com>
2022-10-14 16:51:27 +0200

    Refactor the JAX `Scan` dispatcher

--- aesara/link/jax/dispatch/scan.py
+++ aesara/link/jax/dispatch/scan.py
@@ -1,159 +1,275 @@
-import jax
-import jax.numpy as jnp
+from collections import defaultdict
+from typing import Callable, Dict, List
 
-from aesara.graph.fg import FunctionGraph
+import jax
+
 from aesara.link.jax.dispatch.basic import jax_funcify
 from aesara.scan.op import Scan
-from aesara.scan.utils import ScanArgs
+from aesara.tensor.var import TensorVariable
 
 
 @jax_funcify.register(Scan)
-def jax_funcify_Scan(op, **kwargs):
-    inner_fg = FunctionGraph(op.inputs, op.outputs)
-    jax_at_inner_func = jax_funcify(inner_fg, **kwargs)
+def jax_funcify_Scan(op, node, **kwargs):
+    scan_inner_fn = jax_funcify(op.fgraph)
+    input_taps = {
+        "mit_sot": op.info.mit_sot_in_slices,
+        "sit_sot": op.info.sit_sot_in_slices,
+    }
+
+    # Outer-inputs are the inputs to the `Scan` apply node, built from the
+    # the variables provided by the caller to the `scan` function at construction
+    # time.
+    def parse_outer_inputs(outer_inputs):
+        outer_in = {
+            "n_steps": outer_inputs[0],
+            "sequences": list(op.outer_seqs(outer_inputs)),
+            "mit_mot": list(op.outer_mitmot(outer_inputs)),
+            "mit_sot": list(op.outer_mitsot(outer_inputs)),
+            "nit_sot": list(op.outer_nitsot(outer_inputs)),
+            "sit_sot": list(op.outer_sitsot(outer_inputs)),
+            "shared": list(op.outer_shared(outer_inputs)),
+            "non_sequences": list(op.outer_non_seqs(outer_inputs)),
+        }
+        if len(outer_in["mit_mot"]) > 0:
+            raise NotImplementedError("mit-mot not supported")
+
+        return outer_in
+
+    if op.info.as_while:
+        raise NotImplementedError("While loops are not supported in the JAX backend.")
+    else:
+        return make_jax_scan_fn(
+            scan_inner_fn,
+            parse_outer_inputs,
+            input_taps,
+        )
+
+
+def make_jax_scan_fn(
+    scan_inner_fn: Callable,
+    parse_outer_inputs: Callable[[TensorVariable], Dict[str, List[TensorVariable]]],
+    input_taps: Dict,
+):
+    """Create a `jax.lax.scan` function to perform `Scan` computations.
+
+    `jax.lax.scan` takes an initial `carry` value and a sequence it scans over,
+    or a number of iterations. The first output of the loop body function, the
+    `carry`, is carried over to the next iteration. The second, the `output`, is
+    stacked to the previous outputs. We use this to our advantage to build
+    `Scan` outputs without having to post-process the storage arrays.
+
+    The JAX scan function needs to perform the following operations:
+    1. Extract the inner-inputs;
+    2. Build the initial `carry` and `sequence` values;
+    3. Inside the loop:
+        1. `carry` + sequence elements -> inner-inputs
+        2. inner-outputs -> `carry`
+        3. inner-outputs -> `output`
+    4. Append the last `shared`  value to the stacked `output`s
+
+    """
+
+    def build_jax_scan_inputs(outer_in: Dict):
+        """Build the inputs to `jax.lax.scan` from the outer-inputs."""
+        n_steps = outer_in["n_steps"]
+        sequences = outer_in["sequences"]
+        init_carry = {
+            name: outer_in[name]
+            for name in ["mit_sot", "sit_sot", "shared", "non_sequences"]
+        }
+        init_carry["step"] = 0
+        return n_steps, sequences, init_carry
+
+    def build_inner_outputs_map(outer_in):
+        """Map the inner-output variables to their position in the tuple returned by the inner function.
+
+        Inner-outputs are ordered as follow:
+        - mit-mot-outputs
+        - mit-sot-outputs
+        - sit-sot-outputs
+        - nit-sots (no carry)
+        - shared-outputs
+        [+ while-condition]
+
+        """
+        inner_outputs_names = ["mit_sot", "sit_sot", "nit_sot", "shared"]
+
+        offset = 0
+        inner_output_idx = defaultdict(list)
+        for name in inner_outputs_names:
+            num_outputs = len(outer_in[name])
+            for i in range(num_outputs):
+                inner_output_idx[name].append(offset + i)
+            offset += num_outputs
+
+        return inner_output_idx
+
+    def from_carry_storage(carry, step, input_taps):
+        """Fetch the inner inputs from the values stored in the carry array.
+
+        `Scan` passes storage arrays as inputs, which are then read from and
+        updated in the loop body. At each step we need to read from this array
+        the inputs that will be passed to the inner function.
+
+        This mechanism is necessary because we handle multiple-input taps within
+        the `scan` instead of letting users manage the memory in the use cases
+        where this is necessary.
+
+        """
+
+        def fetch(carry, step, offset):
+            return carry[step + offset]
+
+        inner_inputs = []
+        for taps, carry_element in zip(input_taps, carry):
+            storage_size = -min(taps)
+            offsets = [storage_size + tap for tap in taps]
+            inner_inputs.append(
+                [fetch(carry_element, step, offset) for offset in offsets]
+            )
+
+        return sum(inner_inputs, [])
+
+    def to_carry_storage(inner_outputs, carry, step, input_taps):
+        """Create the new carry array from the inner output
+
+        `Scan` passes storage arrays as inputs, which are then read from and
+        updated in the loop body. At each step we need to update this array
+        with the outputs of the inner function
+
+        """
+        new_carry_element = []
+        for taps, carry_element, output in zip(input_taps, carry, inner_outputs):
+            new_carry_element.append(
+                [carry_element.at[step - tap].set(output) for tap in taps]
+            )
+
+        return sum(new_carry_element, [])
 
     def scan(*outer_inputs):
-        scan_args = ScanArgs(
-            list(outer_inputs), [None] * op.info.n_outs, op.inputs, op.outputs, op.info
-        )
 
-        # `outer_inputs` is a list with the following composite form:
-        # [n_steps]
-        # + outer_in_seqs
-        # + outer_in_mit_mot
-        # + outer_in_mit_sot
-        # + outer_in_sit_sot
-        # + outer_in_shared
-        # + outer_in_nit_sot
-        # + outer_in_non_seqs
-        n_steps = scan_args.n_steps
-        seqs = scan_args.outer_in_seqs
+        outer_in = parse_outer_inputs(outer_inputs)
+        n_steps, sequences, init_carry = build_jax_scan_inputs(outer_in)
+        inner_output_idx = build_inner_outputs_map(outer_in)
 
-        # TODO: mit_mots
-        mit_mot_in_slices = []
+        def scan_inner_in_args(carry, x):
+            """Get inner-inputs from the arguments passed to the `jax.lax.scan` body function.
 
-        mit_sot_in_slices = []
-        for tap, seq in zip(scan_args.mit_sot_in_slices, scan_args.outer_in_mit_sot):
-            neg_taps = [abs(t) for t in tap if t < 0]
-            pos_taps = [abs(t) for t in tap if t > 0]
-            max_neg = max(neg_taps) if neg_taps else 0
-            max_pos = max(pos_taps) if pos_taps else 0
-            init_slice = seq[: max_neg + max_pos]
-            mit_sot_in_slices.append(init_slice)
+            Inner-inputs are ordered as follows:
+            - sequences
+            - mit-mot inputs
+            - mit-sot inputs
+            - sit-sot inputs
+            - shared-inputs
+            - non-sequences
 
-        sit_sot_in_slices = [seq[0] for seq in scan_args.outer_in_sit_sot]
+            """
+            current_step = carry["step"]
 
-        init_carry = (
-            mit_mot_in_slices,
-            mit_sot_in_slices,
-            sit_sot_in_slices,
-            scan_args.outer_in_shared,
-            scan_args.outer_in_non_seqs,
-        )
-
-        def jax_args_to_inner_scan(op, carry, x):
-            # `carry` contains all inner-output taps, non_seqs, and shared
-            # terms
-            (
-                inner_in_mit_mot,
-                inner_in_mit_sot,
-                inner_in_sit_sot,
-                inner_in_shared,
-                inner_in_non_seqs,
-            ) = carry
-
-            # `x` contains the in_seqs
             inner_in_seqs = x
+            inner_in_mit_sot = from_carry_storage(
+                carry["mit_sot"], current_step, input_taps["mit_sot"]
+            )
+            inner_in_sit_sot = from_carry_storage(
+                carry["sit_sot"], current_step, input_taps["sit_sot"]
+            )
+            inner_in_shared = carry.get("shared", [])
+            inner_in_non_sequences = carry.get("non_sequences", [])
 
-            # `inner_scan_inputs` is a list with the following composite form:
-            # inner_in_seqs
-            # + sum(inner_in_mit_mot, [])
-            # + sum(inner_in_mit_sot, [])
-            # + inner_in_sit_sot
-            # + inner_in_shared
-            # + inner_in_non_seqs
-            inner_in_mit_sot_flatten = []
-            for array, index in zip(inner_in_mit_sot, scan_args.mit_sot_in_slices):
-                inner_in_mit_sot_flatten.extend(array[jnp.array(index)])
-
-            inner_scan_inputs = sum(
+            return sum(
                 [
                     inner_in_seqs,
-                    inner_in_mit_mot,
-                    inner_in_mit_sot_flatten,
+                    inner_in_mit_sot,
                     inner_in_sit_sot,
                     inner_in_shared,
-                    inner_in_non_seqs,
+                    inner_in_non_sequences,
                 ],
                 [],
             )
 
-            return inner_scan_inputs
+        def scan_new_carry(carry, inner_outputs):
+            """Create a new carry value from the values returned by the inner function (inner-outputs)."""
+            step = carry["step"]
+            new_carry = {
+                "mit_sot": [],
+                "sit_sot": [],
+                "shared": [],
+                "step": step + 1,
+                "non_sequences": carry["non_sequences"],
+            }
 
-        def inner_scan_outs_to_jax_outs(
-            op,
-            old_carry,
-            inner_scan_outs,
-        ):
-            (
-                inner_in_mit_mot,
-                inner_in_mit_sot,
-                inner_in_sit_sot,
-                inner_in_shared,
-                inner_in_non_seqs,
-            ) = old_carry
+            if "shared" in inner_output_idx:
+                shared_inner_outputs = [
+                    inner_outputs[idx] for idx in inner_output_idx["shared"]
+                ]
+                new_carry["shared"] = shared_inner_outputs
 
-            def update_mit_sot(mit_sot, new_val):
-                return jnp.concatenate([mit_sot[1:], new_val[None, ...]], axis=0)
+            if "mit_sot" in inner_output_idx:
+                mit_sot_inner_outputs = [
+                    inner_outputs[idx] for idx in inner_output_idx["mit_sot"]
+                ]
+                new_carry["mit_sot"] = to_carry_storage(
+                    mit_sot_inner_outputs, carry["mit_sot"], step, input_taps["mit_sot"]
+                )
 
-            inner_out_mit_sot = [
-                update_mit_sot(mit_sot, new_val)
-                for mit_sot, new_val in zip(inner_in_mit_sot, inner_scan_outs)
-            ]
-
-            # This should contain all inner-output taps, non_seqs, and shared
-            # terms
-            if not inner_in_sit_sot:
-                inner_out_sit_sot = []
-            else:
-                inner_out_sit_sot = inner_scan_outs
-            new_carry = (
-                inner_in_mit_mot,
-                inner_out_mit_sot,
-                inner_out_sit_sot,
-                inner_in_shared,
-                inner_in_non_seqs,
-            )
+            if "sit_sot" in inner_output_idx:
+                sit_sot_inner_outputs = [
+                    inner_outputs[idx] for idx in inner_output_idx["sit_sot"]
+                ]
+                new_carry["sit_sot"] = to_carry_storage(
+                    sit_sot_inner_outputs, carry["sit_sot"], step, input_taps["sit_sot"]
+                )
 
             return new_carry
 
-        def jax_inner_func(carry, x):
-            inner_args = jax_args_to_inner_scan(op, carry, x)
-            inner_scan_outs = list(jax_at_inner_func(*inner_args))
-            new_carry = inner_scan_outs_to_jax_outs(op, carry, inner_scan_outs)
-            return new_carry, inner_scan_outs
+        def scan_new_outputs(inner_outputs):
+            """Create a new outer-output value from the outputs of the inner function.
 
-        _, scan_out = jax.lax.scan(jax_inner_func, init_carry, seqs, length=n_steps)
+            Outer-outputs are ordered as follows:
+            - mit-mot-outputs
+            - mit-sot-outputs
+            - sit-sot-outputs
+            - nit-sots
+            - shared-outputs
 
-        # We need to prepend the initial values so that the JAX output will
-        # match the raw `Scan` `Op` output and, thus, work with a downstream
-        # `Subtensor` `Op` introduced by the `scan` helper function.
-        def append_scan_out(scan_in_part, scan_out_part):
-            return jnp.concatenate([scan_in_part[:-n_steps], scan_out_part], axis=0)
+            The shared output corresponds to the last value found in the last
+            carry value returned by `jax.lax.scan`. It is thus not returned in
+            the body function.
 
-        if scan_args.outer_in_mit_sot:
-            scan_out_final = [
-                append_scan_out(init, out)
-                for init, out in zip(scan_args.outer_in_mit_sot, scan_out)
-            ]
-        elif scan_args.outer_in_sit_sot:
-            scan_out_final = [
-                append_scan_out(init, out)
-                for init, out in zip(scan_args.outer_in_sit_sot, scan_out)
-            ]
+            """
+            outer_outputs = []
+            if "mit_sot" in inner_output_idx:
+                outer_outputs.append(
+                    [inner_outputs[idx] for idx in inner_output_idx["mit_sot"]]
+                )
+            if "sit_sot" in inner_output_idx:
+                outer_outputs.append(
+                    [inner_outputs[idx] for idx in inner_output_idx["sit_sot"]]
+                )
+            if "nit_sot" in inner_output_idx:
+                outer_outputs.append(
+                    [inner_outputs[idx] for idx in inner_output_idx["nit_sot"]]
+                )
 
-        if len(scan_out_final) == 1:
-            scan_out_final = scan_out_final[0]
-        return scan_out_final
+            return tuple(sum(outer_outputs, []))
+
+        def body_fn(carry, x):
+            inner_in_args = scan_inner_in_args(carry, x)
+            inner_outputs = scan_inner_fn(*inner_in_args)
+            new_carry = scan_new_carry(carry, inner_outputs)
+            outer_outputs = scan_new_outputs(inner_outputs)
+            return new_carry, outer_outputs
+
+        last_carry, results = jax.lax.scan(
+            body_fn, init_carry, sequences, length=n_steps
+        )
+
+        shared_output = tuple(last_carry["shared"])
+        results = results + shared_output
+
+        if len(results) == 1:
+            return results[0]
+
+        return results
 
     return scan
--- tests/link/jax/test_scan.py
+++ tests/link/jax/test_scan.py
@@ -3,15 +3,321 @@ import pytest
 from packaging.version import parse as version_parse
 
 import aesara.tensor as at
+from aesara import function
+from aesara.compile.mode import Mode
 from aesara.configdefaults import config
 from aesara.graph.fg import FunctionGraph
+from aesara.graph.rewriting.db import RewriteDatabaseQuery
+from aesara.link.jax.linker import JAXLinker
 from aesara.scan.basic import scan
+from aesara.scan.op import Scan
 from aesara.tensor.math import gammaln, log
+from aesara.tensor.random.utils import RandomStream
 from aesara.tensor.type import ivector, lscalar, scalar
 from tests.link.jax.test_basic import compare_jax_and_py
 
 
 jax = pytest.importorskip("jax")
+
+# Disable all optimizations
+opts = RewriteDatabaseQuery(include=[None], exclude=["cxx_only", "BlasOpt"])
+jax_no_opts = Mode(JAXLinker(), opts)
+py_no_opts = Mode("py", opts)
+
+
+def test_while_cannnot_use_all_outputs():
+    """The JAX backend cannot use all the outputs of a while loop.
+
+    Indeed, JAX has fundamental limitations that prevent it from returning
+    all the intermediate results computed in a `jax.lax.while_loop` loop.
+    """
+    res, updates = scan(
+        fn=lambda a_tm1: (a_tm1 + 1, until(a_tm1 > 2)),
+        outputs_info=[{"initial": at.as_tensor(1, dtype=np.int64), "taps": [-1]}],
+        n_steps=5,
+    )
+    with pytest.raises(NotImplementedError):
+        function((), res, updates=updates, mode="JAX")
+
+
+def test_while_only_last_output():
+    """Compile a `Scan` used as a while loop when only the last computed value
+    is used.
+
+    """
+    res, updates = scan(
+        fn=lambda a_tm1: (a_tm1 + 1, until(a_tm1 > 2)),
+        outputs_info=[{"initial": at.as_tensor(1, dtype=np.int64), "taps": [-1]}],
+        n_steps=5,
+    )
+    res = res[-1]
+
+    jax_fn = function((), res, updates=updates, mode="JAX")
+    fn = function((), res, updates=updates)
+    assert np.allclose(fn(), jax_fn())
+
+
+@pytest.mark.xfail(
+    reason="Elemwise{add} transforms concrete values into `TracedArray`s"
+)
+def test_sit_sot():
+    a_at = at.scalar("a", dtype="floatX")
+
+    res, updates = scan(
+        fn=lambda a_tm1: 2 * a_tm1,
+        outputs_info=[{"initial": a_at, "taps": [-1]}],
+        n_steps=3,
+    )
+
+    fn = function((a_at,), res, updates=updates)
+    jax_fn = function((a_at,), res, updates=updates, mode=jax_no_opts)
+    assert np.allclose(fn(1.0), jax_fn(1.0))
+
+
+def test_sit_sot_opt():
+    a_at = at.scalar("a", dtype="floatX")
+
+    res, updates = scan(
+        fn=lambda a_tm1: 2 * a_tm1,
+        outputs_info=[{"initial": a_at, "taps": [-1]}],
+        n_steps=3,
+    )
+
+    jax_fn = function((a_at,), res, updates=updates, mode="JAX")
+    fn = function((a_at,), res, updates=updates)
+    assert np.allclose(fn(1.0), jax_fn(1.0))
+
+
+def test_nit_sot_shared():
+    res, updates = scan(
+        fn=lambda: RandomStream(seed=1930, rng_ctor=np.random.RandomState).normal(
+            0, 1, name="a"
+        ),
+        n_steps=3,
+    )
+
+    jax_fn = function((), res, updates=updates, mode="JAX")
+    res_jax = jax_fn()
+    fn = function((), res, updates=updates)
+    res = fn()
+
+    assert res_jax.shape == res.shape
+    assert not np.all(res_jax == res_jax[0])
+
+
+@pytest.mark.xfail(
+    reason="Elemwise{add} transforms concrete values into `TracedArray`s"
+)
+def test_mit_sot():
+    res, updates = scan(
+        fn=lambda a_tm1: 2 * a_tm1,
+        outputs_info=[
+            {"initial": at.as_tensor([0.0, 1.0], dtype="floatX"), "taps": [-2]}
+        ],
+        n_steps=6,
+    )
+
+    jax_fn = function((), res, updates=updates, mode=jax_no_opts)
+    fn = function((), res, updates=updates)
+    assert np.allclose(fn(), jax_fn())
+
+
+def test_mit_sot_opt():
+    res, updates = scan(
+        fn=lambda a_tm1: 2 * a_tm1,
+        outputs_info=[
+            {"initial": at.as_tensor([0.0, 1.0], dtype="floatX"), "taps": [-2]}
+        ],
+        n_steps=6,
+    )
+
+    jax_fn = function((), res, updates=updates, mode="JAX")
+    fn = function((), res, updates=updates)
+    assert np.allclose(fn(), jax_fn())
+
+
+@pytest.mark.xfail(
+    reason="Elemwise{add} transforms concrete values into `TracedArrays`"
+)
+def test_mit_sot_2():
+    res, updates = scan(
+        fn=lambda a_tm1, b_tm1: (2 * a_tm1, 2 * b_tm1),
+        outputs_info=[
+            {"initial": at.as_tensor(1.0, dtype="floatX"), "taps": [-1]},
+            {"initial": at.as_tensor(0.5, dtype="floatX"), "taps": [-1]},
+        ],
+        n_steps=10,
+    )
+    jax_fn = function((), res, updates=updates, mode=jax_no_opts)
+    fn = function((), res, updates=updates)
+    assert np.allclose(fn(), jax_fn())
+
+
+def test_mit_sot_2_opt():
+    res, updates = scan(
+        fn=lambda a_tm1, b_tm1: (2 * a_tm1, 2 * b_tm1),
+        outputs_info=[
+            {"initial": at.as_tensor(1.0, dtype="floatX"), "taps": [-1]},
+            {"initial": at.as_tensor(0.5, dtype="floatX"), "taps": [-1]},
+        ],
+        n_steps=10,
+    )
+    jax_fn = function((), res, updates=updates, mode="JAX")
+    fn = function((), res, updates=updates)
+    assert np.allclose(fn(), jax_fn())
+
+
+@pytest.mark.xfail(reason="Indexing with non-static values in the optimized graph")
+def test_sequence_opt():
+    a_at = at.dvector("a")
+    res, updates = scan(fn=lambda a_t: 2 * a_t, sequences=a_at)
+    jax_fn = function((a_at,), res, updates=updates, mode="JAX")
+    fn = function((a_at,), res, updates=updates)
+    assert np.allclose(fn(np.arange(10)), jax_fn(np.arange(10)))
+
+
+@pytest.mark.parametrize("jax_mode", ("JAX", jax_no_opts))
+@pytest.mark.parametrize(
+    "fn, sequences, outputs_info, non_sequences, n_steps, input_vals, output_vals, op_check",
+    [
+        # sequences
+        # (
+        #     lambda a_t: 2 * a_t,
+        #     [at.dvector("a")],
+        #     [{}],
+        #     [],
+        #     None,
+        #     [np.arange(10)],
+        #     None,
+        #     lambda op: op.info.n_seqs > 0,
+        # ),
+        # # nit-sot
+        (
+            lambda: at.as_tensor(2.0),
+            [],
+            [{}],
+            [],
+            3,
+            [],
+            None,
+            lambda op: op.info.n_nit_sot > 0,
+        ),
+        # nit-sot, non_seq
+        (
+            lambda c: at.as_tensor(2.0) * c,
+            [],
+            [{}],
+            [at.dscalar("c")],
+            3,
+            [1.0],
+            None,
+            lambda op: op.info.n_nit_sot > 0 and op.info.n_non_seqs > 0,
+        ),
+        # sit-sot
+        # (
+        #     lambda a_tm1: 2 * a_tm1,
+        #     [],
+        #     [{"initial": at.as_tensor(0.0, dtype="floatX"), "taps": [-1]}],
+        #     [],
+        #     3,
+        #     [],
+        #     lambda op: op.info.n_sit_sot > 0,
+        # ),
+        # # sit-sot, while
+        # (
+        #     lambda a_tm1: (a_tm1 + 1, until(a_tm1 > 2)),
+        #     [],
+        #     [{"initial": at.as_tensor(1, dtype=np.int64), "taps": [-1]}],
+        #     [],
+        #     3,
+        #     [],
+        #     None,
+        #     lambda op: op.info.n_sit_sot > 0,
+        # ),
+        # # nit-sot, shared input/output
+        (
+            lambda: RandomStream(seed=1930, rng_ctor=np.random.RandomState).normal(
+                0, 1, name="a"
+            ),
+            [],
+            [{}],
+            [],
+            3,
+            [],
+            [np.array([-0.4587753, -0.89655604, 2.13323775])],
+            lambda op: op.info.n_shared_outs > 0,
+        ),
+        # mit-sot (that's also a type of sit-sot)
+        # (
+        #     lambda a_tm1: 2 * a_tm1,
+        #     [],
+        #     [{"initial": at.as_tensor([0.0, 1.0], dtype="floatX"), "taps": [-2]}],
+        #     [],
+        #     6,
+        #     [],
+        #     None,
+        #     lambda op: op.info.n_mit_sot > 0,
+        # ),
+        # # mit-sot
+        # (
+        #     lambda a_tm1, b_tm1: (2 * a_tm1, 2 * b_tm1),
+        #     [],
+        #     [
+        #         {"initial": at.as_tensor(1.0, dtype="floatX"), "taps": [-1]},
+        #         {"initial": at.as_tensor(0.3, dtype="floatX"), "taps": [-1]},
+        #     ],
+        #     [],
+        #     10,
+        #     [],
+        #     None,
+        #     lambda op: op.info.n_mit_sot > 0,
+        # ),
+    ],
+)
+def test_xit_xot_types(
+    jax_mode,
+    fn,
+    sequences,
+    outputs_info,
+    non_sequences,
+    n_steps,
+    input_vals,
+    output_vals,
+    op_check,
+):
+    """Test basic xit-xot configurations."""
+    res, updates = scan(
+        fn,
+        sequences=sequences,
+        outputs_info=outputs_info,
+        non_sequences=non_sequences,
+        n_steps=n_steps,
+        strict=True,
+    )
+
+    if not isinstance(res, list):
+        res = [res]
+
+    # Get rid of any `Subtensor` indexing on the `Scan` outputs
+    res = [r.owner.inputs[0] if not isinstance(r.owner.op, Scan) else r for r in res]
+
+    scan_op = res[0].owner.op
+    assert isinstance(scan_op, Scan)
+
+    _ = op_check(scan_op)
+
+    if output_vals is None:
+        compare_jax_and_py(
+            ((sequences + non_sequences), res),
+            input_vals,
+            updates=updates,
+        )
+    else:
+        jax_fn = function(
+            (sequences + non_sequences), res, updates=updates, mode=jax_mode
+        )
+        res_vals = jax_fn(*input_vals)
+        assert np.allclose(res_vals, output_vals)
 
 
 @pytest.mark.xfail(
@@ -144,3 +450,29 @@ def test_jax_scan_tap_output():
 
     test_input_vals = [np.array(10.0).astype(config.floatX)]
     compare_jax_and_py(out_fg, test_input_vals)
+
+
+def test_scan_multiple_none_output():
+    A = at.dvector("A")
+
+    def power_step(prior_result, x):
+        return prior_result * x, prior_result * x * x, prior_result * x * x * x
+
+    result, _ = scan(
+        power_step,
+        non_sequences=[A],
+        outputs_info=[at.ones_like(A), None, None],
+        n_steps=3,
+    )
+
+    FunctionGraph([A], result)
+    test_input_vals = (np.array([1.0, 2.0]),)
+
+    jax_fn = function((A,), result, mode="JAX")
+    jax_res = jax_fn(*test_input_vals)
+
+    fn = function((A,), result)
+    res = fn(*test_input_vals)
+
+    for output_jax, output in zip(jax_res, res):
+        assert np.allclose(jax_res, res)
