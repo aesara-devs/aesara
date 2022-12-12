fd1c4b7a8209b1bb8b3b23ef2c8193ea0047c94d
Rémi Louf <remilouf@gmail.com>
2022-12-12 14:42:01 +0100

    Add rewrites to re-express boolean indexing logic

--- aesara/tensor/rewriting/__init__.py
+++ aesara/tensor/rewriting/__init__.py
@@ -1,6 +1,9 @@
 import aesara.tensor.rewriting.basic
 import aesara.tensor.rewriting.elemwise
 import aesara.tensor.rewriting.extra_ops
+
+# Register JAX specializations
+import aesara.tensor.rewriting.jax
 import aesara.tensor.rewriting.math
 import aesara.tensor.rewriting.shape
 import aesara.tensor.rewriting.special
