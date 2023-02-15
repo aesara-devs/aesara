92522541da13474f8f453bdb470331429c3a38d4
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2023-02-15 17:13:50 -0600

    Support sub-package-level deprecations in aesara.scalar

--- aesara/scalar/__init__.py
+++ aesara/scalar/__init__.py
@@ -1,2 +1,28 @@
-from .basic import *
-from .math import *
+from typing import List, Tuple
+
+from aesara.scalar.basic import *
+from aesara.scalar.math import *
+
+
+# isort: off
+from aesara.scalar.basic import DEPRECATED_NAMES as BASIC_DEPRECATIONS
+
+# isort: on
+
+DEPRECATED_NAMES: List[Tuple[str, str, object]] = BASIC_DEPRECATIONS
+
+
+def __getattr__(name):
+    """Intercept module-level attribute access of deprecated symbols.
+
+    Adapted from https://stackoverflow.com/a/55139609/3006474.
+
+    """
+    from warnings import warn
+
+    for old_name, msg, old_object in DEPRECATED_NAMES:
+        if name == old_name:
+            warn(msg, DeprecationWarning, stacklevel=2)
+            return old_object
+
+    raise AttributeError(f"module {__name__} has no attribute {name}")
