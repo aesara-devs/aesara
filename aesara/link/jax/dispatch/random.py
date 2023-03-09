5dc9c804c2200f0dbc1f1c2d0eca76f4c55be363
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-03-09 12:22:32 +0530

    Remove StandardNormalRV Op and use partial func

--- aesara/link/jax/dispatch/random.py
+++ aesara/link/jax/dispatch/random.py
@@ -138,7 +138,6 @@ def jax_sample_fn_generic(op):
 @jax_sample_fn.register(aer.LaplaceRV)
 @jax_sample_fn.register(aer.LogisticRV)
 @jax_sample_fn.register(aer.NormalRV)
-@jax_sample_fn.register(aer.StandardNormalRV)
 def jax_sample_fn_loc_scale(op):
     """JAX implementation of random variables in the loc-scale families.
 
