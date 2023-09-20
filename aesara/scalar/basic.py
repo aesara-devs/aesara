e9a8840dbdfec044026f7813c48131d07da3e296
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2023-09-20 14:31:01 -0500

    Update NumPy variadic name from product to prod

--- aesara/scalar/basic.py
+++ aesara/scalar/basic.py
@@ -1889,7 +1889,7 @@ class Mul(ScalarOp):
     commutative = True
     associative = True
     nfunc_spec = ("multiply", 2, 1)
-    nfunc_variadic = "product"
+    nfunc_variadic = "prod"
 
     def impl(self, *inputs):
         return np.prod(inputs)
